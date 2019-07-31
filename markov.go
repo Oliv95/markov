c4c74fa409ff6816fee6372dc5c0499b2f623d5a
Axel Olivecrona <axel.olivecrona1995@gmail.com>
2019-07-31 12:51:05 +0200

    some refactoring and added mutex locks

--- markov.go
+++ markov.go
@@ -5,6 +5,7 @@ import "fmt"
 import "errors"
 import "math/rand"
 import "time"
+import "sync"
 
 // State represents a state in a markov chain.
 // The Data field needs be comparable, it will be used as a map key
@@ -12,35 +13,47 @@ type State struct {
 	Data interface{}
 }
 
-// TransitionTable stores all the transitions in the markov chain
-// Each state is assosicated with the all transitions from that state
-type TransitionTable = map[State][]State
+// Graph represents the markov chain
+type Graph struct {
+	table map[State][]State
+	lock  *sync.RWMutex
+}
 
-// CreateEmptyTable returns a empty transitionTable
-func CreateEmptyTable() TransitionTable {
-	return make(TransitionTable)
+// CreateEmptyGraph returns a empty graph
+func CreateEmptyGraph() Graph {
+	graph := Graph{
+		map[State][]State{},
+		&sync.RWMutex{},
+	}
+	return graph
 
 }
 
 // AddTransition adds the transition
 // 	from -> to
 // into the markov chain or if the transition already exists, increases it's weight
-func AddTransition(table *TransitionTable, from State, to State) {
+func AddTransition(graph *Graph, from State, to State) {
 	// TODO change this to only store ratio, no need to store duplicates
 	// TODO make thread safe
-	(*table)[from] = append((*table)[from], to)
+	graph.lock.Lock()
+	defer graph.lock.Unlock()
+	graph.table[from] = append(graph.table[from], to)
 }
 
 // RandomState returns a random state from the table
 // or if the table is empty or nil errors
-func RandomState(table *TransitionTable) (State, error) {
-	emptyOrNil := table == nil || len(*table) == 0
+func RandomState(graph *Graph) (State, error) {
+	emptyOrNil := graph == nil || len(graph.table) == 0
 	if emptyOrNil {
-		return State{}, fmt.Errorf("Table cannot be empty or nil: %v", table)
+		return State{}, fmt.Errorf("Graph cannot be empty or nil: %v", graph.table)
 	}
 	rand.Seed(time.Now().UTC().UnixNano())
-	keys := make([]State, 0, len(*table))
-	for k := range *table {
+
+	graph.lock.RLock()
+	defer graph.lock.RUnlock()
+
+	keys := make([]State, 0, len(graph.table))
+	for k := range graph.table {
 		keys = append(keys, k)
 	}
 	nbrKeys := len(keys)
@@ -52,13 +65,17 @@ func RandomState(table *TransitionTable) (State, error) {
 // Transition returns the new state after one time step from the given state
 //
 // Errors if the table is nil, current does not exist in the table or does not have any transitions defined
-func Transition(table *TransitionTable, current State) (*State, error) {
-	if table == nil {
-		return nil, errors.New("Table cannot be nil")
+func Transition(graph *Graph, current State) (*State, error) {
+	if graph == nil {
+		return nil, errors.New("Graph cannot be nil")
 	}
-	transitions, ok := (*table)[current]
+
+	graph.lock.RLock()
+	defer graph.lock.RUnlock()
+
+	transitions, ok := (graph.table)[current]
 	if !ok {
-		return nil, fmt.Errorf("State %v not in table %v", current, *table)
+		return nil, fmt.Errorf("State %v not in table %v", current, graph.table)
 	}
 	nbrTransitions := len(transitions)
 	// Checks if there is no possible state to move to
